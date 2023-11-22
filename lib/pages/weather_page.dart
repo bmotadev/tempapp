a40595e4f5bb557e053f13598281ba69f4bf155d
Bruno Mota <bmotadev@gmail.com>
2023-11-22 19:08:07 -0300

    :sparkles: feat: Adiciona animações e condições

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:tempapp/services/weather_service.dart';
 
 import '../models/weather_model.dart';
@@ -35,7 +36,30 @@ class _WeatherPageState extends State<WeatherPage> {
     }
   }
 
-  // animações
+  // animações do clima
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sol.json'; // padrão é ensolarado
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/solchuva.sjon';
+      case 'thunderstorm':
+        return 'assets/tempestade';
+      case 'clear':
+        return 'assets/sol.json';
+      default:
+        return 'assets/sol.json';
+    }
+  }
 
   // iniciar estado
   @override
@@ -49,6 +73,7 @@ class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlue[100],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -56,8 +81,14 @@ class _WeatherPageState extends State<WeatherPage> {
             // nome da cidade
             Text(_weather?.cityName ?? "Carregando cidade..."),
 
+            // animação
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
             // temperatura
-            Text('${_weather?.temperature.round()}°C')
+            Text('${_weather?.temperature.round()}°C'),
+
+            // condição do clima
+            Text(_weather?.mainCondition ?? "")
           ],
         ),
       ),
